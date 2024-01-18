6715558172dc0d80cb884d76e19cfc1376195735
finger-soggy <157001458+finger-soggy@users.noreply.github.com>
2024-01-18 16:10:11 +0800

    Add files via upload

---
+++ HashTable.java
@@ -0,0 +1,50 @@
+class HashTable {
+    int[] slot = new int[3];
+    public void put(int k) {
+        int s=0;
+        boolean loaded = false;
+        while (!loaded) {
+            if (slot[s] == 0) {
+                slot[s] = k;
+                loaded = true;
+            }
+            else {
+                s++;
+            }
+        }
+    }
+
+    public void remove(int k){
+        int s=0;
+        boolean found = false;
+        while (!found && s<3) {
+            if (slot[s] == k) {
+                slot[s] = 0;
+                found = true;
+            }
+            else {
+                s++;
+            }
+        }
+        if (!found) {
+            System.out.println("The key you want to remove is not in the hash table");
+        }
+    }
+
+    public int get(int k) {
+        int s=0;
+        boolean found = false;
+        while (!found && s<3) {
+            if (slot[s] == k) {
+                found = true;
+                System.out.println("Key found");
+                return slot[s];
+            }
+            else {
+                s++;
+            }
+        }
+        System.out.println("The key does not exist");
+        return 0;
+    }
+}
